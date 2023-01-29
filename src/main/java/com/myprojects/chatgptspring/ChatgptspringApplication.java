7ba7d467bdd89889667df986cec507fcac0ff8e2
Manikanta Bonam <mabonam@deloitte.com>
2023-01-29 18:11:04 +0530

    initial commit

---
+++ src/main/java/com/myprojects/chatgptspring/ChatgptspringApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.chatgptspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptspringApplication.class, args);
+	}
+
+}
